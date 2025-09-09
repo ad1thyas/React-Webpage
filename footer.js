8f1f2d7272d1833119cb0caeaf04b99de0a19790
Adithya S <adithyaptr77@gmail.com>
2025-09-09 18:09:57 +0530

    Create footer.js

---
+++ footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "../components/footer.css";
+export default function footer() {
+  return (
+    <footer className="footer">  
+      <a href="https://en.wikipedia.org/wiki/Swami_Vivekananda">Click here for more information</a>
+    </footer>
+  );
+}
