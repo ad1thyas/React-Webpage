f2940f14611597c23805450ac15e625591a50ce8
Adithya S <adithyaptr77@gmail.com>
2025-09-09 18:03:34 +0530

    Create maincontent.js

---
+++ maincontent.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "./maincontent.css";
+import vivekananda from "./vivekananda.png";
+function MainContent() {
+  return (
+    <div className="main-content">
+      <img src={vivekananda} alt="Vivekananda" />
+      <div className="main-text">
+    <u><center><h1>Swami Vivekananda</h1></center></u>
+          <p>
+            <br/>
+            Swami Vivekananda was one of India’s most influential spiritual leaders, philosophers, and social reformers. 
+            Born as Narendranath Datta on January 12, 1863, in Kolkata, He was deeply inspired by the teachings of Sri Ramakrishna Paramahamsa, his spiritual guru.
+        he was deeply inspired by the teachings of Sri Ramakrishna Paramahamsa, his spiritual guru.</p>
+        <br/>
+        <p>
+ Born into an aristocratic Bengali Kayastha family in Calcutta (now Kolkata),Vivekananda showed an early inclination towards religion and spirituality. At the age of 18, he met Ramakrishna and became his devoted disciple, and later took up the vows of a sannyasin (renunciate).
+ Following Ramakrishna’s death, Vivekananda travelled extensively across the Indian subcontinent as a wandering monk, gaining first-hand knowledge of the often harsh living conditions endured by the Indian masses under then British India, he sought a way to alleviate their suffering by establishing social services but lacked capital. 
+        </p>
+      </div>
+    </div>
+  );
+}
+export default MainContent;
+
+maincontent.css:
+.main-content {
+  display: flex;
+  align-items: flex-start;
+  padding: 20px;
+  gap: 20px; }
+.main-content img {
+  width: 135px;
+  height: auto;
+  border-radius: 8px;
+  margin-top: 30px; 
+}
+.main-text h1  {
+  font-size: 1.8rem;
+  font-weight: bold;
+  margin-bottom: 10px;
+  font-family: "times new roman";
+}
+.main-text p {
+  font-size: 1rem;
+  line-height: 1.6;
+  text-align: justify;
+  font-family: 'Times New Roman';
+}
